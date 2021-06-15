a338afc285a74dec8baa8ca025e0f8cb4d9d52b7
PankajMSharma <pankajmsharma357@hotmail.com>
2021-06-15 23:27:37 +0530

    Added frontend view

--- backend/serve.js
+++ backend/serve.js
@@ -2,6 +2,7 @@ require('dotenv').config({ path: './.env' });
 const express = require('express');
 const { HTTP_STATUS } = require('./consts/http-error-status');
 const cors = require('cors');
+var path = require('path');
 
 const customResponse = require('./response');
 const DEFAULT_PORT = 4201;
@@ -19,11 +20,27 @@ app.use(express.json());
 // Allow request from all public origins
 app.use(cors());
 
+/**
+ * Set View Engine to pug
+**/
+app.set('view engine', 'pug')
+
+/* 
+ * Location for views
+**/
+app.set('views', './backend/views')
+
+/**
+ * Set path to static resource files
+**/
+app.use("/static", express.static(path.join(__dirname, "public")));
+
 /** 
  * Test Request Handler
 **/
 app.get('', function (req, res) {
-    res.sendStatus(200)
+    res.render('index', { title: 'Hey', message: 'Hello there!',
+    tableRows: [{ srNum: 1, shortUrl: 'sdfssd', fullUrl: 'http://facebook.com', frequency: 23 }] })
 })
 
 /**
@@ -31,7 +48,8 @@ app.get('', function (req, res) {
 **/
 function globalErrorHandler (err, req, res, next) {
     console.log('Into Global Error Handeler ', res.status)
-    const error = customResponse.create(null, 'Internal Server Error', 500, err);
+    const error = customResponse.create(null, HTTP_STATUS.INTERNAL_SERVER_ERROR.message,
+                                        HTTP_STATUS.INTERNAL_SERVER_ERROR.code, err);
 
     // Send error if header not sent already
     if (!res.headerSent) return res.status(500).json(error);
